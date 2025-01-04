e52bbe6c01b3ac4350e1e1d787a00a0fbb4a244a
Astha Singh <77485251+Asthasingh222@users.noreply.github.com>
2025-01-04 18:38:53 +0530

    Fix tailwind.config.js to use ESM imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
 export default {
 	darkMode: ["class"],
 	content: [
@@ -68,5 +69,5 @@ export default {
 		},
 	  },
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [tailwindcssAnimate],
   };

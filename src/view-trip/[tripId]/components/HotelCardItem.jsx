4450923286208ac2d93ba008f68c97f9f719f608
Astha Singh <77485251+Asthasingh222@users.noreply.github.com>
2025-01-04 19:12:52 +0530

    blank photo fixed

--- src/view-trip/[tripId]/components/HotelCardItem.jsx
+++ src/view-trip/[tripId]/components/HotelCardItem.jsx
@@ -29,7 +29,7 @@ const HotelCardItem = ({ hotel}) => {
      <Link to={'https://www.google.com/maps/search/?api=1&query='+hotel?.name+","+hotel?.address} target="_blank">
             <div className="hover:scale-105 transition-all cursor-pointer">
 
-                <img src={photoUrl} className="rounded-xl h-[160px]"/>
+                <img src={photoUrl?photoUrl:'/tripinfo2.avif'} className="rounded-xl h-[160px]"/>
                 <div className="my-2 flex flex-col gap-2">
                     <h2 className="font-medium">{hotel?.name}</h2>
                     <h2 className="text-xs text-gray-700 ">📍{hotel?.address}</h2>
